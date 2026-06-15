8081aaaf24e71591b817a0c4cb986d8c12cc1c8f
tanmay-hm <tanmaymodi221@gmail.com>
2026-06-15 19:41:40 +0530

    Added dielectric material

--- include/Vector3.h
+++ include/Vector3.h
@@ -111,8 +111,13 @@ inline vec3 random_on_hemisphere(const vec3& normal){
         return -output;
     }
 }
-
 inline vec3 reflect(const vec3& incident, const vec3& normal){
     vec3 reflected = incident - 2*dot(incident,normal)*normal;
     return reflected;
+}
+inline vec3 refract(const vec3& uv, const vec3& n, double relative_refractive_index) {
+    auto cos_theta = std::fmin(dot(-uv, n), 1.0);
+    vec3 r_out_perp =  relative_refractive_index * (uv + cos_theta*n);
+    vec3 r_out_parallel = -std::sqrt(std::fabs(1.0 - r_out_perp.squared_length())) * n;
+    return r_out_perp + r_out_parallel;
 }
